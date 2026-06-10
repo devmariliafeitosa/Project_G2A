510e5390e03fb1e7236df4061fcde3190bb9ec98
Lupercio <lupercionetof@gmail.com>
2026-06-10 20:51:57 -0300

    feat: JWT Auth initial configuration

--- backend/g2a/g2a/urls.py
+++ backend/g2a/g2a/urls.py
@@ -14,8 +14,11 @@ Class-based views
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("api")),
+    path("login", LoginView.as_view(), name="login"), # Integrar com as telas do Frontend
 ]
